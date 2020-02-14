68a020469e1df098ce9ab3bcf1e27da51bfa38e9
heilorg <lhs01201@gmail.com>
2020-02-14 12:44:46 +0900

    add music upload, play

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
-import { MdAdd } from "react-icons/md";
+import {
+    MdAdd,
+    MdPlayArrow,
+    MdStop,
+    MdSkipPrevious,
+    MdSkipNext,
+    MdPause
+} from "react-icons/md";
 import "../style/home.css";
 
 class Home extends Component {
     constructor(props) {
         super(props);
-        this.state = { name: "" };
+        this.state = { name: "", songs: [] };
 
         this.handleLogout = this.handleLogout.bind(this);
         this.onDragOver = this.onDragOver.bind(this);
         this.onFileDrop = this.onFileDrop.bind(this);
+        this.musicPlay = this.musicPlay.bind(this);
     }
 
     handleLogout() {
@@ -37,7 +45,42 @@ class Home extends Component {
         event.stopPropagation();
         event.persist();
 
-        console.log(event);
+        let loadSongs = event.dataTransfer.files;
+        let songs = [];
+        for (let i = 0; i < loadSongs.length; i++) {
+            let song = loadSongs[i];
+            if (song.type.substring(0, 5) !== "audio") {
+                alert("음악파일만 업로드 가능합니다.");
+                continue;
+            }
+
+            let songURL = URL.createObjectURL(song);
+            let songData = { title: song.name, data: songURL };
+            songs.push(songData);
+            fetch("api/song/add", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(songData)
+            })
+                .catch(err => {
+                    console.log(err);
+                    alert("잘못된 파일입니다.");
+                })
+                .then(res => res.json())
+                .then(console.log("추가 완료"));
+        }
+        let state = this.state.songs;
+        this.setState({
+            songs: state.concat(songs)
+        });
+    }
+
+    musicPlay() {
+        let song = new Audio();
+        song.src = this.state.songs[0].data;
+        song.play();
     }
 
     componentDidMount() {
@@ -55,6 +98,14 @@ class Home extends Component {
                 } else if (res.user !== undefined) {
                     let name = res.user.name;
                     this.setState({ name });
+                    fetch("api/song/get")
+                        .catch(err => {
+                            console.log(err);
+                        })
+                        .then(res => res.json())
+                        .then(res => {
+                            this.setState({ songs: res });
+                        });
                 }
             });
     }
@@ -73,24 +124,38 @@ class Home extends Component {
                         onDragOver={this.onDragOver}
                         onDrop={this.onFileDrop}
                     >
-                        <div className="list-item">
-                            <div>music name</div>
-                            <div>other data</div>
-                        </div>
-                        <div className="list-item">
-                            <div>music name</div>
-                            <div>other data</div>
-                        </div>
-                        <div className="list-item">
-                            <div>music name</div>
-                            <div>other data</div>
-                        </div>
+                        {this.state.songs.map((song, idx) => (
+                            <div className="list-item" key={idx}>
+                                <div>{song.title}</div>
+                            </div>
+                        ))}
                         <div className="list-item add-item">
                             <MdAdd />
                             <input type="file" />
                         </div>
                     </div>
-                    <div className="effecter"></div>
+                    <div className="effecter">
+                        <div className="effect-tool">
+                            <div className="effect-tool-button">
+                                <button>
+                                    <MdSkipPrevious />
+                                </button>
+                                <button onClick={this.musicPlay}>
+                                    <MdPlayArrow />
+                                </button>
+                                <button>
+                                    <MdPause />
+                                </button>
+                                <button>
+                                    <MdStop />
+                                </button>
+                                <button>
+                                    <MdSkipNext />
+                                </button>
+                            </div>
+                            <div className="effect-tool-progress">div</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
